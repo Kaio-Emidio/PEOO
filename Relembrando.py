014e355d4e07cb727fa4dccc0d28b150b757510a
Kaio-Emidio <kaio.emidio@escolar.ifrn.edu.br>
2024-03-05 17:12:41 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   Relembrando.py

---
+++ Relembrando.py
@@ -0,0 +1,54 @@
+# Questão 1
+x = 10
+y = 5.5
+nome = 'João'
+
+print(x, y, nome)
+
+# Questão 2
+
+inteiro = int(10)
+flutuante = float(5.5)
+string = str('Olá, Mundo!')
+booleans = bool(True)
+
+print(inteiro, flutuante, string, booleans)
+
+# Questão 3
+
+a = 10
+b = 2
+
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+módulo = a % b
+potência = a ** b
+
+print(soma, subtração, multiplicação, divisão, módulo, potência)
+
+# Questão 4
+
+a = 10
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# Questão 5
+
+n = 10
+print(n)
+n += 5
+print(n)
+n -= 3
+print(n)
+n *= 2
+print(n)
+n /= 4
+print(n)
